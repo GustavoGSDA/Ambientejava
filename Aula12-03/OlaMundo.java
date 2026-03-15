bd1b11674f1db0e8162de1d525793187b86511ec
GustavoGSDA <gustavodalmeida79@gmail.com>
2026-03-15 12:56:56 -0300

    Update OlaMundo.java

--- Aula12-03/OlaMundo.java
+++ Aula12-03/OlaMundo.java
@@ -1,5 +1,15 @@
-public class OlaMundo {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Meu primeiro código!");
+
+import java.util.Scanner;
+
+public class BoasVindas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do cliente: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Seja bem-vindo, " + nome + "!");
+
+        scanner.close();
     }
 }
