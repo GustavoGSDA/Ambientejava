0dd6e57bebad76fbd11d21c6471a2a114f073a32
GustavoGSDA <gustavodalmeida79@gmail.com>
2026-03-15 19:16:33 -0300

    Change welcome message in OlaMundo.java
    Updated welcome message to use 'Bem-vindo' instead of 'Seja bem-vindo'.

--- Aula12-03/OlaMundo.java
+++ Aula12-03/OlaMundo.java
@@ -1,14 +1,14 @@
-
 import java.util.Scanner;
-
 public class BoasVindas {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Solicita o nome do cliente
         System.out.print("Digite o nome do cliente: ");
         String nome = scanner.nextLine();
 
-        System.out.println("Seja bem-vindo, " + nome + "!");
+        // Exibe a mensagem de boas-vindas
+        System.out.println("Bem-vindo, " + nome + "!");
 
         scanner.close();
     }
